8d28973722601fa204ae10840f2b7388a6717279
Michael Giuffrida <michaelg@michaelg.us>
2014-05-18 19:43:28 -0700

    Use .mp3 files in IE

--- game/states/preload.js
+++ game/states/preload.js
@@ -1,5 +1,24 @@
 'use strict';
 
+// Returns the version of Internet Explorer or a -1
+// (indicating the use of another browser).
+function getInternetExplorerVersion()
+{
+  var rv = -1;
+  var ua = window.navigator.userAgent;
+  var re;
+  if (window.navigator.appName === 'Microsoft Internet Explorer') {
+    re = new RegExp('MSIE ([0-9]{1,}[\\.0-9]{0,})');
+    if (re.exec(ua) !== null)
+      rv = parseFloat(RegExp.$1);
+  } else if (window.navigator.appName === 'Netscape') {
+    re = new RegExp('Trident/.*rv:([0-9]{1,}[\\.0-9]{0,})');
+    if (re.exec(ua) !== null)
+      rv = parseFloat(RegExp.$1);
+  }
+  return rv;
+}
+
 function Preload() {
   this.asset = null;
   this.ready = false;
@@ -20,12 +39,11 @@ Preload.prototype = {
 
     this.load.image('bullet', 'assets/images/bullet.png');
 
-    this.load.audio('shoot', 'assets/sounds/shoot-sin.wav');
-    this.load.audio('shoot-soft', 'assets/sounds/shoot-triangle.wav');
-    this.load.audio('explosion', 'assets/sounds/explosion.wav');
-    this.load.audio('enemy-hit', 'assets/sounds/enemy-hit.wav');
-
-//    this.load.bitmapFont('hiscorefont', 'assets/fonts/hiscoreletters.png');
+    var extension = (getInternetExplorerVersion() === -1) ? 'wav' : 'mp3';
+    this.load.audio('shoot', 'assets/sounds/shoot-sin.' + extension);
+    this.load.audio('shoot-soft', 'assets/sounds/shoot-triangle.' + extension);
+    this.load.audio('explosion', 'assets/sounds/explosion.' + extension);
+    this.load.audio('enemy-hit', 'assets/sounds/enemy-hit.' + extension);
   },
 
   create: function() {
